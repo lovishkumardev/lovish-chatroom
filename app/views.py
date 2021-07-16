b05179603973957cfaf650669f4c982edaa1a531
lovishkumardev <lovishkumardev@gmail.com>
2021-07-16 21:41:41 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+
+from django.http import HttpResponse, JsonResponse
+
+from .models import *
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def send(request):
+    msg = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=msg, user=username, room=room_id)
+    new_message.save()
+    
+    return HttpResponse('Message Send Succesfully.')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+    msgs = Message.objects.filter(room=room_details.id)
+
+    return JsonResponse({"messages":list(msgs.values())})
+
+def room(request, room):
+    username = request.GET.get('username') # Taking username from the url
+    room_details = Room.objects.get(name=room)
+
+    return render(request, 'room.html', {
+        'username': username,
+        'room': room, 
+        'room_details': room_details
+    })
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect(f'/{room}/?username={username}')
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect(f'/{room}/?username={username}')
